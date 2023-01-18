91ca4b57bd33860a4ae604b0b217dd01a54fc851
mateuso12 <mateuso12@hotmail.com.br>
2023-01-18 19:47:11 -0300

    Initial Commit

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import { Habit } from './components/Habit'
+import './styles/global.css'
+
+function App() {
+ return(
+  <div className="bg-zinc-900 flex w-full h-full text-white">
+  <Habit />
+  <Habit />
+  <Habit />
+  <Habit />
+  </div>
+ )
+}
+
+export default App
