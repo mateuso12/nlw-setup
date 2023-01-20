168bc3f5a267ccd971d760df9b473309cc3de590
mateuso12 <mateuso12@hotmail.com.br>
2023-01-19 22:14:34 -0300

    new: create page of summary days

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,15 @@
-import { Habit } from './components/Habit'
+// import { Habit } from './components/Habit'
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 import './styles/global.css'
 
-function App() {
- return(
-  <div className="bg-zinc-900 flex w-full h-full text-white">
-  <Habit />
-  <Habit />
-  <Habit />
-  <Habit />
-  </div>
- )
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
+  )
 }
-
-export default App
--- web/src/components/Habit/index.tsx
+++
@@ -1,5 +0,0 @@
-export function Habit() {
-  return (  
-    <h1>Teste</h1>
-  );
-}
