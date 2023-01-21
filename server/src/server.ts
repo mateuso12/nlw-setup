67629476a59a0c66c741e05490d870f6bb1cf133
mateuso12 <mateuso12@hotmail.com.br>
2023-01-21 11:18:50 -0300

    fix: fix backend app register

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors'
+import { appRoutes } from './Routes';
 
 const app = Fastify();
 
 app.register(cors)
+app.register(appRoutes)
 
 app.listen({
   port:3333
